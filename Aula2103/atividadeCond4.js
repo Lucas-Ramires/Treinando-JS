027983d0f622f6e036a447c5f68fe03a1958cff4
kotecbr@outlook.com <kotecbr@outlook.com>
2024-03-21 17:38:10 -0300

    Atualizando

--- Aula2103/atividadeCond4.js
+++ Aula2103/atividadeCond4.js
@@ -8,22 +8,34 @@ grupo = leia.question("Digite se o animal e Vertebrado ou Invertebrado: ");
 tipodeAnimal = leia.question("Digite se e Ave, Mamifero, Inseto ou Anelideo: ");
 alimentacao = leia.question("Digite se e Carnivoro,Onivoro,Herbivoro ou Hematofago: ");
 
-if(grupo === "Vertebrado" && tipodeAnimal === "Ave" && alimentacao === "Carnivoro"){
-    console.log("Aguia")
-}else if(grupo === "Vertebrado" && tipodeAnimal === "Ave" && alimentacao === "Onivoro"){
-    console.log("Pomba")
-}else if(grupo === "Vertebrado" && tipodeAnimal === "Mamifero" && alimentacao === "Onivoro"){
-    console.log("Homem")
-}else if(grupo === "Vertebrado" && tipodeAnimal === "Mamifero" && alimentacao === "Herbivoro"){
-    console.log("Vaca")
-}else if(grupo === "Invertebrado" && tipodeAnimal === "Inseto" && alimentacao === "Hematofago"){
-    console.log("Pulga")
-}else if(grupo === "Invertebrado" && tipodeAnimal === "Inseto" && alimentacao === "Herbivoro"){
-    console.log("Largata")
-}else if(grupo === "Invertebrado" && tipodeAnimal === "Anelideo" && alimentacao === "Hematofago"){
-    console.log("Sanguessuga")
-}else if(grupo === "Invertebrado" && tipodeAnimal === "Anelideo" && alimentacao === "Onivoro"){
-    console.log("Minhoca")
-}else{
-    console.log("Preste atenção nas letras Maiusculas");
+if(grupo === Vertebrado){
+    if(tipodeAnimal === "Ave"){
+        if(alimentacao === "Carnivoro"){
+            console.log("Águia");
+        }else{
+            console.log("Pomba");
+        }
+    }else{
+        if(alimentacao === "Herbivoro"){
+            console.log("Vaca");
+        }else{
+            console.log("homem")
+        }
+    }
+}else {
+    if(grupo === Invertebrado){
+        if(tipodeAnimal === "Inseto"){
+            if(alimentacao === "Herbivoro"){
+                console.log("Pulga");
+            }else{
+                console.log("Largata");
+            }
+        }else{
+            if(alimentacao === "Onivoro"){
+                console.log("Minhoca");
+            }else{
+                console.log("Sanguessuga")
+            }
+        }
+    }
 }
